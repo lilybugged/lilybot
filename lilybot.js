e96b815858d45fc7c652a9d7f009904f512b94d1
Lilianna <lilianna@swansontech.net>
2016-11-03 23:12:26 -0400

    commit

--- lilybot.js
+++ lilybot.js
@@ -359,6 +359,7 @@ bot.on("message",function(message){
 	else if (message.content.substring(0,3)=="\\js"){
 		//message.delete();
 		message.channel.sendMessage(eval(message.content.substring(3)));
+		message.delete();
 	}
 	else if (message.content.toLowerCase()===("\\cmdpls")||message.content.toLowerCase()===("\\cmdspls")){
 		//message.reply( "have sum cmds");
